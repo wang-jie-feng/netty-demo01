c2b9c0d104bdc0217537624be3d76b378e412059
jiefeng <jiefengfire@163.com>
2019-11-15 16:29:05 +0800

    first commit

---
+++ src/main/java/com/fire/test/server/HelloServerHandler.java
@@ -0,0 +1,36 @@
+package com.fire.test.server;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+import io.netty.util.ReferenceCountUtil;
+
+public class HelloServerHandler extends ChannelInboundHandlerAdapter {
+    /**
+     * 收到数据时调用
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        try {
+            ByteBuf in = (ByteBuf)msg;
+            System.out.print(in.toString(CharsetUtil.UTF_8));
+        } finally {
+            // 抛弃收到的数据
+            ReferenceCountUtil.release(msg);
+        }
+
+//        ctx.write(msg);
+//        ctx.flush();
+    }
+
+    /**
+     * 当Netty由于IO错误或者处理器在处理事件时抛出异常时调用
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        // 当出现异常就关闭连接
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
